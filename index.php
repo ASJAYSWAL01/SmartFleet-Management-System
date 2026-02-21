03d2ee2250bfb71f5a4cce5a5e210f318940cfa6
Yash Bhavsar_1020 <bhavsaryash823@gmail.com>
2026-02-21 10:40:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h1>PHP is Working Successfully 🚀</h1>";
 echo "<p>Server Time: " . date("Y-m-d H:i:s") . "</p>";
-echo "<h1>Jaldi karo kal subhe panwel nikal na hai mereko.</h1>";
+echo "<marquee><h1>Jaldi karo kal subhe panwel nikal na hai mereko.</h1></marquee>";
 echo "<marquee>AAJ HI PROJECT SUBMIT KARNA HAI!!!</marquee>";
 ?>
