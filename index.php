339c59e10be644fed66fd6ce65316cb528e1e228
Yash Bhavsar_1020 <bhavsaryash823@gmail.com>
2026-02-21 10:35:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,4 @@
 echo "<h1>PHP is Working Successfully 🚀</h1>";
 echo "<p>Server Time: " . date("Y-m-d H:i:s") . "</p>";
 
-
-echo "<h1>Hello Time table aa gaya mid ka LODI SARVA VISHVAVIDYALAYA KA.</h1>";
-
 ?>
